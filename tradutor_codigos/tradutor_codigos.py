f0e3ade73abe8bf822bec52c3464a4da67d1fdf9
Davi Cerqueira <104117559+daviscerqueira@users.noreply.github.com>
2025-02-14 17:43:38 -0300

    Add files via upload

--- tradutor_codigos/tradutor_codigos.py
+++ tradutor_codigos/tradutor_codigos.py
@@ -1,36 +1,30 @@
-import keyboard, json, pyperclip, pyttsx3
-from pygame import mixer
-mixer.init()
-pytts = pyttsx3.init()
+import keyboard, json, pyperclip
 
 
-#///////////////////////#
+# /////////////////////// #
 
 type_hotkey = "/"
 
 translate_hotkey = ';'
 
-#///////////////////////#
-
-tts_letter = ""
-tts_str = ""
+# /////////////////////// #
 
 
-with open("tradutor_codigos/tabela.json", "r") as f:
-    table = json.load(f)
+with open("tabela.json", "r") as f:
+    data = json.load(f)
 
 while True:
     tp = input("bin / morse?: ")
+
     if (tp != "morse" and tp != "bin"):
-        print("inválido")
+        print("Inválido\n")
+
     else:
+        table = data[tp]
         break
 
 while True:
-    keyboard.release(type_hotkey)
-    tts = mixer.Sound("tradutor_codigos/audios/comecar.WAV")
-    tts.play()
-    print("Pressione a tecla de atalho para começar")
+    print("Pressione a tecla para começar ou traduzir\n")
 
     while True:
         read = keyboard.read_hotkey(False)
@@ -38,52 +32,44 @@ while True:
         if (read == type_hotkey):
             keyboard.send('backspace')
             break
+
         if (read == translate_hotkey):
+            keyboard.send('backspace')
             cb = pyperclip.paste()
             
-            try:
-                teste = int(cb[0])
-                data = table['bin']
-            except:
-                data = table['morse']
+            if (cb[0].isnumeric()):
+                cb_table = data['bin']
+            else:
+                cb_table = data['morse']
 
             tts_str = ""
 
-            for char in cb:
-                if (char != " "):
-                    tts_letter += char
-                else:
-                    tts_letter += " "
-                    for a in data:
-                        if (tts_letter == data[a]):
-                            if(a == "space"):
-                                tts_str += " "
-                            else:
-                                tts_str += a
-                            break
-                    tts_letter = ""
-            tts.stop()
-            print(tts_str)
-            pytts.say(tts_str)
-            pytts.runAndWait()
+            cb = cb.split()
 
-    tts.stop()
-    tts = mixer.Sound("tradutor_codigos/audios/traduzindo.WAV")
-    tts.play()
-    print("traduzindo")
+            reverse_table = {v: k for k, v in cb_table.items()}
+
+            for i in cb:
+                i += " "
+                try:
+                    if (not reverse_table[i] == 'space'):
+                        tts_str += reverse_table[i]
+                    else:
+                        tts_str += " "
+                except:
+                    continue
+                
+            print(f"Texto copiado: {tts_str}\n")
+
+
+    print("traduzindo...\n")
 
     while True:   
-        tecla = keyboard.read_event(True)
-        try:
-            if (tecla.event_type == 'down'):
-                if (tp == "bin"):
-                    keyboard.write(table[tp][tecla.name])
-                else:
-                    keyboard.write(table[tp][(tecla.name).lower()])
-            else:
-                tp += table #forçar um erro
-        except:
-            if (tecla.name == 'backspace' and tecla.event_type == 'down'):
-                keyboard.send('backspace')
-            elif (tecla.name == type_hotkey and tecla.event_type == 'up'):
-                break
+        tecla = keyboard.read_event()
+
+        if (tecla.event_type == 'down' and tecla.name in table):
+            keyboard.send('backspace')
+            keyboard.write(table[tecla.name])
+        
+        elif (tecla.name == type_hotkey and tecla.event_type == 'up'):
+            keyboard.send('backspace')
+            break
